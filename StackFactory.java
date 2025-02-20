e73f9140d17f3a24c6c24911012c0ac9d7792767
Diego-glitch-cloud <dcalderon918@gmail.com>
2025-02-19 19:39:59 -0600

    implementación de StackFactory

---
+++ StackFactory.java
@@ -0,0 +1,15 @@
+public class StackFactory {
+  
+    public static <T> IStack<T> createStack(String stackType, String listType) {
+        if (stackType.equalsIgnoreCase("ArrayList")) {
+            return new ArrayListStack<>();
+        } else if (stackType.equalsIgnoreCase("Vector")) {
+            return new VectorStack<>();
+        } else if (stackType.equalsIgnoreCase("List")) {
+            // Por simplicidad, se retorna la implementación ListStack.
+            return new ListStack<>();
+        } else {
+            throw new IllegalArgumentException("Tipo de pila no reconocido: " + stackType);
+        }
+    }
+}
